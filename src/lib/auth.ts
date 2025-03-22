ccb0edbc5d0721712ba881e420ad71e9d56e8bab
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-22 14:40:04 +0500

    implement email verification and password reset functionality in auth module

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "~/db/schema";
 
 import { db } from "~/db";
+import { sendEmail } from "~/utils/mail";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -12,7 +13,28 @@ export const auth = betterAuth({
       ...schema,
     },
   }),
+  trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
+    autoSignIn: true,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Reset your password",
+        text: `Click the link to reset your password: ${url}`,
+      });
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `Click the link to verify your email: ${url}`,
+      });
+    },
   },
 });
